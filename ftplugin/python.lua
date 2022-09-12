bf3b3101387e3baf569ae2cdb6a7eee3a6befc89
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-09-12 13:22:10 +0300

    Updates

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -3,4 +3,4 @@ local path = vim.api.nvim_buf_get_name(0)
 vim.keymap.set('n', '<CR>', function()
   vim.fn.system { "tmux", "send-keys", "-t", "debug:1.1", "clear\n" }
   vim.fn.system { "tmux", "send-keys", "-t", "debug:1.1", "python " .. path .. "\n" }
-end, {})
+end, {buffer = true})
