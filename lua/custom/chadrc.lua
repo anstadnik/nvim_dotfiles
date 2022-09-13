063ff5bd5353ff90c9685d8da22bcb9bef712bfc
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-09-13 10:19:35 +0300

    Fix for new chadrc

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -5,27 +5,34 @@ M.ui = {
   theme = "onenord",
   tabufline = {
     lazyload = false,
-  }
+  },
 }
 
 local pluginConfs = require "custom.plugins.configs"
 
 M.plugins = {
-  remove = {
-    "NvChad/nvim-colorizer.lua",
-    "williamboman/mason.nvim",
-    -- "neovim/nvim-lspconfig"
-  },
-  override = {
-    ["nvim-treesitter/nvim-treesitter"] = pluginConfs.treesitter,
-    ["hrsh7th/nvim-cmp"] = pluginConfs.nvimcmp,
-    ["nvim-telescope/telescope.nvim"] = pluginConfs.telescope,
-    ["kyazdani42/nvim-tree.lua"] = pluginConfs.nvimtree,
-    ["lewis6991/gitsigns.nvim"] = pluginConfs.gitsigns,
-    ["L3MON4D3/LuaSnip"] = pluginConfs.luasnip,
-  },
-  user = require "custom.plugins",
+  -- remove
+  ["NvChad/nvim-colorizer.lua"] = false,
+  ["williamboman/mason.nvim"] = false,
 }
+
+local over_plugins = {
+  { "nvim-treesitter/nvim-treesitter", pluginConfs.treesitter },
+  { "hrsh7th/nvim-cmp", pluginConfs.nvimcmp },
+  { "nvim-telescope/telescope.nvim", pluginConfs.telescope },
+  { "kyazdani42/nvim-tree.lua", pluginConfs.nvimtree },
+  { "lewis6991/gitsigns.nvim", pluginConfs.gitsigns },
+  { "L3MON4D3/LuaSnip", pluginConfs.luasnip },
+}
+
+for _, v in ipairs(over_plugins) do
+  M.plugins[v[1]] = { override_options = v[2] }
+end
+
+local cust_plugins = require "custom.plugins"
+for k, v in pairs(cust_plugins) do
+  M.plugins[k] = v
+end
 
 M.mappings = require "custom.mappings"
 
