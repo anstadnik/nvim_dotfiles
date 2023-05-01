db4e35badccc614f7c076b510a04f1810303ca68
Stadnik Andrii <stadnikandriy1@gmail.com>
2023-05-01 16:29:03 +0300

    Update catppuccin plugin appearance configuration

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -2,13 +2,53 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
+    lazy=false,
+    priority=1000,
     config = function()
-      require("catppuccin").setup {
-        native_lsp = { enabled = true },
-        -- navic = { enabled = false },
-      }
-      vim.cmd.colorscheme "catppuccin"
+      require("catppuccin").setup {}
+      --   flavour = "mocha", -- latte, frappe, macchiato, mocha
+      --   background = { -- :h background
+      --     light = "latte",
+      --     dark = "mocha",
+      --   },
+      --   transparent_background = false,
+      --   show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+      --   term_colors = false,
+      --   dim_inactive = {
+      --     enabled = false,
+      --     shade = "dark",
+      --     percentage = 0.15,
+      --   },
+      --   no_italic = false, -- Force no italic
+      --   no_bold = false, -- Force no bold
+      --   styles = {
+      --     comments = { "italic" },
+      --     conditionals = { "italic" },
+      --     loops = {},
+      --     functions = {},
+      --     keywords = {},
+      --     strings = {},
+      --     variables = {},
+      --     numbers = {},
+      --     booleans = {},
+      --     properties = {},
+      --     types = {},
+      --     operators = {},
+      --   },
+      --   color_overrides = {},
+      --   custom_highlights = {},
+      --   integrations = {
+      --     cmp = true,
+      --     gitsigns = true,
+      --     nvimtree = true,
+      --     telescope = true,
+      --     notify = false,
+      --     mini = false,
+      --     -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+      --   },
+      -- }
       vim.opt.background = "light"
+      vim.cmd.colorscheme "catppuccin"
     end,
   },
   -- {
